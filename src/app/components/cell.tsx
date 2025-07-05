7b2d7211a81c69c3c47fc0ecf89ca2964cfc6c65
riku546 <riku62560@gmail.com>
2025-07-05 23:49:36 +0900

    一マスを定義するCell componentを作成

---
+++ src/app/components/cell.tsx
@@ -0,0 +1,29 @@
+import type { MazeElement, MazeSizeType } from '../../Maze/type';
+
+const Cell = ({ mazeType, cell }: { mazeType: MazeSizeType; cell: MazeElement }) => {
+  const backgroundColor: Record<MazeElement, string> = {
+    wall: 'black',
+    route: 'green',
+    path: 'white',
+    start: 'red',
+    goal: 'red',
+  };
+
+  const sizes: Record<MazeSizeType, { width: string; height: string }> = {
+    easy: { width: '30px', height: '30px' },
+    medium: { width: '12px', height: '12px' },
+    hard: { width: '8px', height: '8px' },
+    extreme: { width: '7px', height: '7px' },
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: backgroundColor[cell],
+        width: sizes[mazeType].width,
+        height: sizes[mazeType].height,
+      }}
+    />
+  );
+};
+
+export default Cell;
