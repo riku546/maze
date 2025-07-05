202137bc2c2ad6a62b1b565b1f8a1ea8c39ae17a
riku546 <riku62560@gmail.com>
2025-07-05 23:52:39 +0900

    迷路に関する型を作成

--- src/Maze/type.ts
+++ src/Maze/type.ts
@@ -1,2 +1,13 @@
-export type MazeElement = 'wall' | 'path' | 'start' | 'end';
+export type MazeElement = 'wall' | 'path' | 'goal' | 'start' | 'route';
 export type Maze = MazeElement[][];
+
+
+export const mazeSizeOptions = {
+  easy: 21,
+  medium: 51,
+  hard: 81,
+  extreme: 101,
+} as const;
+
+export type MazeSizeType = keyof typeof mazeSizeOptions;
+export type MazeSizes = (typeof mazeSizeOptions)[MazeSizeType];
