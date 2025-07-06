1d714c97d91a2874e7860bb1a0ea16ed1246bc6c
riku546 <riku62560@gmail.com>
2025-07-06 16:58:27 +0900

    stepInto関数のリファクタリング

--- src/Maze/lib.ts
+++ src/Maze/lib.ts
@@ -6,99 +6,67 @@ const directions = {
   down: { y: 1, x: 0 },
   left: { y: 0, x: -1 },
   right: { y: 0, x: 1 },
-};
+} as const;
 
-// interface StepIntoWayByDirectionType {
-//   frontIsPath: {
-//     up: 'up';
-//     down: 'down';
-//     left: 'left';
-//     right: 'right';
-//   };
-//   frontIsWall: {
-//     up: 'right';
-//     down: 'left';
-//     left: 'up';
-//     right: 'down';
-//   };
-//   leftIsPath: {
-//     up: 'left';
-//     down: 'right';
-//     left: 'down';
-//     right: 'up';
-//   };
-// }
-
-// const stepIntoWayByDirectionType: StepIntoWayByDirectionType = {
-//   frontIsPath: {
-//     up: 'up',
-//     down: 'down',
-//     left: 'left',
-//     right: 'right',
-//   },
-//   frontIsWall: {
-//     up: 'right',
-//     down: 'left',
-//     left: 'up',
-//     right: 'down',
-//   },
-//   leftIsPath: {
-//     up: 'left',
-//     down: 'right',
-//     left: 'down',
-//     right: 'up',
-//   },
-// };
-
-const leftByDirection = {
+const left = {
   up: { y: 0, x: -1 },
   down: { y: 0, x: 1 },
   left: { y: 1, x: 0 },
   right: { y: -1, x: 0 },
-};
+} as const;
 
-interface FrontWallByDirection {
-  up: 'right';
-  down: 'left';
-  left: 'up';
-  right: 'down';
-}
-
-interface LeftIsPathByDirection {
-  up: { y: 0; x: -1; direction: 'left' };
-  down: { y: 0; x: 1; direction: 'right' };
-  left: { y: 1; x: 0; direction: 'down' };
-  right: { y: -1; x: 0; direction: 'up' };
-}
-
-const frontWallByDirection: FrontWallByDirection = {
+const frontWall = {
   up: 'right',
   down: 'left',
   left: 'up',
   right: 'down',
-};
+} as const;
 
-const leftIsPathByDirection: LeftIsPathByDirection = {
-  up: { y: 0, x: -1, direction: 'left' },
-  down: { y: 0, x: 1, direction: 'right' },
-  left: { y: 1, x: 0, direction: 'down' },
-  right: { y: -1, x: 0, direction: 'up' },
-};
+const upperLeft = {
+  up: 'left',
+  down: 'right',
+  left: 'down',
+  right: 'up',
+} as const;
+
+interface FrontWall {
+  type: 'changeDirection';
+  newPositionY: number;
+  newPositionX: number;
+
+  newDirection: UserDirection;
+}
+
+interface LeftIspath {
+  type: 'forwardAndChangeDicrection';
+  newPositionY: number;
+  newPositionX: number;
+
+  newDirection: UserDirection;
+}
+interface Forward {
+  type: 'forward';
+  newPositionY: number;
+  newPositionX: number;
+
+  newDirection: UserDirection;
+}
+
+type StepIntoResponse = Forward | FrontWall | LeftIspath;
 
 export const findRoute = (maze: Maze, userInfo: UserInfo) => {
   maze[0][0] = 'path';
+
   let { positionX, positionY, directionType } = structuredClone(userInfo);
+
   const route = [{ y: 0, x: 0, direction: 'right' }];
+
   while (maze[positionY][positionX] !== 'goal') {
-    const nextInfo = stepInto(maze, positionX, positionY, directionType);
+    const newUserInfo = stepInto(maze, positionY, positionX, directionType);
 
-    if (!nextInfo) {
-      return route;
-    }
-
-    positionX = nextInfo.newPositionX;
-    positionY = nextInfo.newPositionY;
-    directionType = nextInfo.directionType as UserDirection;
+    positionX = newUserInfo.newPositionX;
+    positionY = newUserInfo.newPositionY;
+    directionType = newUserInfo.newDirection;
 
     route.push({ y: positionY, x: positionX, direction: directionType });
   }
@@ -107,111 +75,57 @@ export const findRoute = (maze: Maze, userInfo: UserInfo) => {
 };
 
 const isOutInMaze = (maze: Maze, positionX: number, positionY: number) => {
-  const isOut =
-    positionX < 0 || positionX >= maze.length || positionY < 0 || positionY >= maze.length;
-  return isOut;
+  return positionX < 0 || positionX >= maze.length || positionY < 0 || positionY >= maze.length;
 };
 
 export const stepInto = (
   maze: Maze,
-  positionX: number,
   positionY: number,
-  direction: UserDirection,
-) => {
-  const leftBlockX = positionX + leftByDirection[direction].x;
-  const leftBlockY = positionY + leftByDirection[direction].y;
+  positionX: number,
+  di: UserDirection,
+): StepIntoResponse => {
+  const nextPositionY = positionY + directions[di].y;
+  const nextPositionX = positionX + directions[di].x;
 
-  if (isOutInMaze(maze, leftBlockX, leftBlockY)) {
-    const nextPositionY = positionY + directions[direction].y;
-    const nextPositionX = positionX + directions[direction].x;
+  const isFrontOutWall = isOutInMaze(maze, nextPositionX, nextPositionY);
 
-    if (isOutInMaze(maze, nextPositionX, nextPositionY)) {
-      return {
-        newPositionX: positionX,
-        newPositionY: positionY,
-        directionType: frontWallByDirection[direction],
-      };
-    } else if (maze[nextPositionY][nextPositionX] === 'goal') {
-      return {
-        newPositionX: nextPositionX,
-        newPositionY: nextPositionY,
-        directionType: direction,
-      };
-    } else {
-      if (
-        maze[nextPositionY][nextPositionX] === 'path' ||
-        maze[nextPositionY][nextPositionX] === 'route'
-      ) {
-        return {
-          newPositionX: nextPositionX,
-          newPositionY: nextPositionY,
-          directionType: direction,
-        };
-      } else if (maze[nextPositionY][nextPositionX] === 'wall') {
-        return {
-          newPositionX: positionX,
-          newPositionY: positionY,
-          directionType: frontWallByDirection[direction],
-        };
-      }
-    }
-  } else if (maze[leftBlockY][leftBlockX] === 'wall') {
-    const nextPositionY = positionY + directions[direction].y;
-    const nextPositionX = positionX + directions[direction].x;
+  const isFrontWall = isFrontOutWall ? false : maze[nextPositionY][nextPositionX] === 'wall';
 
-    if (isOutInMaze(maze, nextPositionX, nextPositionY)) {
-      return {
-        newPositionX: positionX,
-        newPositionY: positionY,
-        directionType: frontWallByDirection[direction],
-      };
-    } else if (maze[nextPositionY][nextPositionX] === 'goal') {
-      return {
-        newPositionX: nextPositionX,
-        newPositionY: nextPositionY,
-        directionType: direction,
-      };
-    } else {
-      if (
-        maze[nextPositionY][nextPositionX] === 'path' ||
-        maze[nextPositionY][nextPositionX] === 'route'
-      ) {
-        return {
-          newPositionX: nextPositionX,
-          newPositionY: nextPositionY,
-          directionType: direction,
-        };
-      } else if (maze[nextPositionY][nextPositionX] === 'wall') {
-        return {
-          newPositionX: positionX,
-          newPositionY: positionY,
-          directionType: frontWallByDirection[direction],
-        };
-      }
-    }
-  } else if (maze[leftBlockY][leftBlockX] === 'path' || maze[leftBlockY][leftBlockX] === 'route') {
-    const nextPositionY = positionY + leftIsPathByDirection[direction].y;
-    const nextPositionX = positionX + leftIsPathByDirection[direction].x;
-    if (isOutInMaze(maze, nextPositionX, nextPositionY)) {
-      return {
-        newPositionX: positionX,
-        newPositionY: positionY,
-        directionType: frontWallByDirection[direction],
-      };
-    } else if (maze[nextPositionY][nextPositionX] === 'goal') {
-      return {
-        newPositionX: nextPositionX,
-        newPositionY: nextPositionY,
-        directionType: direction,
-      };
-    } else {
-      return {
-        newPositionX: nextPositionX,
-        newPositionY: nextPositionY,
-        directionType: leftIsPathByDirection[direction].direction,
-      };
-    }
+  const leftPositionY = nextPositionY + left[di].y;
+  const leftPositionX = nextPositionX + left[di].x;
+  const isLeftOut = isOutInMaze(maze, leftPositionX, leftPositionY);
+  const isLeftPath = isLeftOut ? false : maze[leftPositionY][leftPositionX] !== 'wall';
+
+  if (isFrontOutWall) {
+    return {
+      type: 'changeDirection',
+      newPositionY: positionY,
+      newPositionX: positionX,
+      newDirection: frontWall[di],
+    };
+  } else if (isFrontWall) {
+    return {
+      type: 'changeDirection',
+      newPositionY: positionY,
+      newPositionX: positionX,
+      newDirection: frontWall[di],
+    };
+  } else if (isLeftPath) {
+    return {
+      type: 'forwardAndChangeDicrection',
+      newPositionY: nextPositionY,
+      newPositionX: nextPositionX,
+      newDirection: upperLeft[di],
+    };
   }
+
+  //真っ直ぐ進む
+  return {
+    type: 'forward',
+    newPositionY: nextPositionY,
+    newPositionX: nextPositionX,
+    newDirection: di,
+  };
 };
 
 export const displayRouteInMaze = (
@@ -225,20 +139,16 @@ export const displayRouteInMaze = (
   const route = findRoute(maze, initialUserInfo);
   const nextUserInfo = stepInto(
     maze,
-    userInfo.positionX,
     userInfo.positionY,
+    userInfo.positionX,
     userInfo.directionType,
   );
-
-  if (!nextUserInfo) {
-    return mazeCopy;
-  }
 
   const breakIndex = route.findIndex(
     (r) =>
       r.y === nextUserInfo.newPositionY &&
       r.x === nextUserInfo.newPositionX &&
-      r.direction === nextUserInfo.directionType,
+      r.direction === nextUserInfo.newDirection,
   );
 
   const passedRoutes = breakIndex === -1 ? [...route] : route.slice(0, breakIndex);
